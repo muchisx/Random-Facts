2ac7d2ecd9efcfbbf4e04a7ecc14e07bf5b1b396
muchisx <mnm.soul@gmail.com>
2022-06-10 00:57:38 -0500

    🤷🏻‍♂️

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// ---------------- QUOKKA --------------------------------
+
+({
+    "plugins": ["jsdom-quokka-plugin"],
+    "jsdom": {
+        "file": "./1. Escuela Desarrollo Web/32-API-REST/project/index.html",
+    },
+    "env": {
+        "params": {
+            "runner": "--experimental-fetch"
+        }
+    }
+})
+
+// ---------------- QUOKKA --------------------------------
+
+const API_URL = 'https://uselessfacts.jsph.pl/'
+const OPTIONS = [
+    "random",
+    ".json",
+    "?language=en"
+]
+
+
+const fetchPhrase = async (API_URL) => {
+
+    let res = await fetch(`${API_URL}${OPTIONS[0]}${OPTIONS[1]}${OPTIONS[2]}`);
+    let data = await res.json();
+
+    console.log(data, '    <- data');
+    return data
+}
+
+
+const changeRandomFact = async () => {
+
+    const data = await fetchPhrase(API_URL);
+
+    P_RANDOM_FACT.innerText = `“${data.text}”`
+}
+
+const P_RANDOM_FACT = document.getElementById('random-fact')
